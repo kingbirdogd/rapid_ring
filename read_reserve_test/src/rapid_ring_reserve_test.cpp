d0ad3054632e4734fd0e5e6acea70ed786e6a5ae
David AU <kingbirdogd@gmail.com>
2020-05-19 02:22:03 +0800

    add disruptor basic try test

--- read_reserve_test/src/rapid_ring_reserve_test.cpp
+++ read_reserve_test/src/rapid_ring_reserve_test.cpp
@@ -1,5 +1,93 @@
 #include <rapid_ring/ring_buffer_queue.hpp>
+#include <rapid_ring/ring_buffer_disruptor.hpp>
 #include <iostream>
+#include <thread>
+#include <exception>
+
+
+void ring_buffer_disruptor_test()
+{
+	using queue = rapid_ring::mp_ring_buffer_disruptor<int, 16>;
+	using comsumer = queue::comsumer_mt;
+	std::vector<int> cnt1;
+	std::vector<int> cnt2;
+	queue q;
+	comsumer c1(q);
+	comsumer c2(q);
+	auto cfg = q.get_configure();
+	cfg.x_depends_y(c1, q);
+	cfg.x_depends_y(c2, q);
+	cfg.commit();
+	std::thread t1([&]()
+	{
+		int v = 0;
+		while (v < 2048)
+		{
+			int v2 = 0;
+			if(c1.try_dequeue(v2))
+			{
+				if (1 != v2 - v)
+				{
+					throw std::runtime_error(std::string("T1 fail,v2:") + std::to_string(v2) + ",v:" + std::to_string(v));
+				}
+				v = v2;
+				std::cout << "T1 dequeue:" << v << std::endl;
+			}
+		}
+	});
+	std::thread t2([&]()
+	{
+		int v = 0;
+		while (v < 2048)
+		{
+			int v2 = 0;
+			if(c2.try_dequeue(v2))
+			{
+				if (1 != v2 - v)
+				{
+					throw std::runtime_error(std::string("T2 fail,v2:") + std::to_string(v2) + ",v:" + std::to_string(v));
+				}
+				v = v2;
+				std::cout << "T2 dequeue:" << v << std::endl;
+			}
+		}
+	});
+	for (int i = 1; i <= 2048; ++i)
+	{
+		std::cout << "Enqueue:" << i << std::endl;
+		q.enqueue(i);
+	}
+	t1.join();
+	t2.join();
+	std::cout << "Disruptor test success!" << std::endl;
+}
+
+void ring_buffer_queue_test()
+{
+	using queue = rapid_ring::spsc_ring_buffer_queue<int, 64>;
+	std::vector<int> cnt1;
+	queue q;
+	std::thread t1([&]()
+	{
+		int v = 0;
+		while (v < 2048)
+		{
+			if(q.try_dequeue(v))
+			{
+				std::cout << "T1 dequeue:" << v << std::endl;
+				cnt1.push_back(v);
+			}
+		}
+	});
+	for (int i = 1; i <= 2048; ++i)
+	{
+		std::cout << "Enqueue:" << i << std::endl;
+		q.enqueue(i);
+	}
+	t1.join();
+	std::cout << "cnt1:" << cnt1.size() << std::endl;
+	std::cout << "Queue test success!" << std::endl;
+}
 
 int main(void)
 {
@@ -39,6 +127,8 @@ int main(void)
 	q.enqueue(18);
 	q.enqueue(19);
 	q.enqueue(20);
+	ring_buffer_queue_test();
+	ring_buffer_disruptor_test();
 	return 0;
 }
 
